127c955ebbb7f7c1f7a7e8da04e4e0d7d85d6f5f
ayshamarty <ayshamarty@gmail.com>
2019-08-05 15:47:25 +0100

    first commit

---
+++ src/main/java/com/bae/accountmaker/AccountMakerApplication.java
@@ -0,0 +1,13 @@
+package com.bae.accountmaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountMakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountMakerApplication.class, args);
+	}
+
+}
